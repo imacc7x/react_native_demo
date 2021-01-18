dc2aa517ea7c2736beed5f9d6b924d1985776024
Siripong <siripong.chu@ku.th>
2021-01-19 02:57:46 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import {View, Text, FlatList, StyleSheet, Alert} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+import uuid from 'uuid-random';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'HTML'},
+    {id: uuid(), text: 'CSS'},
+    {id: uuid(), text: 'JS'},
+    {id: uuid(), text: 'PHP'}
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != id);
+    });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'กรุณาใส่ข้อมูล', {text: 'ok'});
+    } else {
+      setItems((prevItems) => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} /> 
+      <FlatList data={items}
+        renderItem={({item}) => (<ListItem item={item} deleteItem={deleteItem} />)}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
+
+export default App;
